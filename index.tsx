120e8f6559cb0d306037eeb489abe15bd35c719c
Sharvanibu24 <147300477+Sharvanibu24@users.noreply.github.com>
2025-04-15 21:25:07 +0530

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,15 @@
+// This page simply redirects to the HomePage component
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Index = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/");
+  }, [navigate]);
+
+  return null;
+};
+
+export default Index;
